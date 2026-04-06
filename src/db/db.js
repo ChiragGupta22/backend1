f9a853d2f49d465ea44fdfc8eadf5fdf2f7ca652
Chirag <chiragguptaworks@gmail.com>
2026-04-06 22:57:02 +0530

    now check the filesissss

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose
-      .connect(process.env.MONGO_URI)
-      .then(() => console.log("DB Connected ✅"))
-      .catch((err) => console.log("DB Error ❌", err));
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("data is connected");
   } catch (err) {
     console.log("error to have Your fetched data", err);
     process.exit(1);
