b215f268bb46c0983c9959336ba91499e4127ace
Chirag <chiragguptaworks@gmail.com>
2026-04-08 17:14:43 +0530

    yes done

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://frontend1-pink-pi.vercel.app",
+    origin: "https://frontend1-beige-nine.vercel.app",
     credentials: true,
   }),
 );
